ebec29e5b3efaf0cb710cd50361acbe518275c74
BOMBEIROSSP\32263773886 <victoramaro@policiamilitar.sp.gov.br>
2021-12-22 15:05:46 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,7 +45,7 @@ export class AppComponent implements OnInit {
         "cancel": "Cancelar",
         "dayNames": ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
         "dayNamesShort": ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
-        "dayNamesMin": ["Do","Se","Te","Qua","Qui","Se","Sa"],
+        "dayNamesMin": ["D","S","T","Q","Q","S","S"],
         "monthNames": ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
         "monthNamesShort": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul", "Ago", "Set", "Out", "Nov", "Dez"],
         "today": "Hoje",
